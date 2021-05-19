3ad0531c1b4215cd004306f326fe0ad9ba24a302
suresh <shansuresh25@gmail.com>
2021-05-19 22:03:48 +0530

    new program added

---
+++ codePractice2/numtriangle3.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the number: "))
+
+for i in range(num):
+    for j in range(i+1):
+        x = 0
+        for k in range(j):
+            x = x + num - k
+        if j % 2 == 0:
+            print(x+i-j+1, end="  ")
+        else:
+            print(x + num- i, end="  ")
+    print()
